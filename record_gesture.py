0d46922e23c1b2d51485c7f09bc6aa04084526bc
dean <dean.fl20@student.unibi.ac.id>
2025-01-24 09:53:33 +0700

    add classfication model and notebook to train model
    record gesture: add drawing style

--- record_gesture.py
+++ record_gesture.py
@@ -48,7 +48,7 @@ while True:
         # Hand detection
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing_style.get_default_hand_landmarks_style(), mp_drawing_style.get_default_hand_connections_style())
                 landmarks = [[lm.x, lm.y,lm.z] for lm in hand_landmarks.landmark]
                 
                 base_palm = landmarks[0]
